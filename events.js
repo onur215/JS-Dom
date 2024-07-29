59d84c2c5dcdf8aa4a5c2e87277813f6f6c903e1
onur <onurb215@hotmail.com>
2024-07-29 21:46:47 +0300

    first

--- events.js
+++ events.js
@@ -3,3 +3,9 @@
 //* ===============================================
 
 console.log("****** EVENTS *******")
+
+const section = document.querySelector(".product-list")
+console.log(section.firstChild);
+
+const deneme = document.querySelector("main").firstElementChild.firstElementChild
+console.log(deneme);
